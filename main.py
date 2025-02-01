444d2be45d27b9bc55cefcef85866ffc39014b99
projecthub12 <142118290+projecthub12@users.noreply.github.com>
2025-02-01 12:55:27 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,16 +8,29 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    """Return a friendly HTTP greeting.
-
-    Returns:
-        A string with the words 'Hello World!'.
-    """
     return "Hello World!"
 
+@app.route('/hello2', methods=['GET'])
+def hello_world2():
+    return jsonify({'message': 'Hello, Aruni'})
+
+@app.route('/hello', methods=['GET'])
+def hello_world():
+    return jsonify({'message': 'Hello, World!'})
+
+@app.route('/sum', methods=['POST'])
+def calculate_sum():
+    data = request.get_json()
+    if 'num1' not in data or 'num2' not in data:
+        return jsonify({'error': 'Missing parameters'}), 400
+    
+    try:
+        num1 = float(data['num1'])
+        num2 = float(data['num2'])
+        result = num1 + num2 + 2
+        return jsonify({'sum': result})
+    except ValueError:
+        return jsonify({'error': 'Invalid input, numbers expected'}), 400
 
 if __name__ == "__main__":
-    # This is used when running locally only. When deploying to Google App
-    # Engine, a webserver process such as Gunicorn will serve the app. You
-    # can configure startup instructions by adding `entrypoint` to app.yaml.
     app.run(host="127.0.0.1", port=8080, debug=True)
